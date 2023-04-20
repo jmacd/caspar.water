9a94e505727a0baa401544808cee19daaa52698e
Joshua MacDonald <jmacd@lightstep.com>
2023-04-19 23:53:51 -0700

    configure sysv init script for edgemon

--- cmd/edgemon/main.go
+++ cmd/edgemon/main.go
@@ -5,45 +5,113 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"sync"
 	"time"
 
 	probing "github.com/prometheus-community/pro-bing"
 )
 
 var port = flag.Int("port", 8888, "metrics port")
-var probe = flag.String("probe", "192.168.0.40", "monitor ipv4")
+var target = flag.String("target", "192.168.0.40", "monitor target")
+var window = flag.Int("window", 600, "size of window in seconds")
+var interval = flag.Int("interval", 10, "interval in seconds")
+
+func fmtDur(x time.Duration) string {
+	s := x.String()
+	if strings.HasSuffix(s, "0s") {
+		return s[:len(s)-2]
+	}
+	return s
+}
 
 func main() {
-	flag.Parse()
-
 	startTime := time.Now()
 
-	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	flag.Parse()
 
-		pinger, err := probing.NewPinger("www.google.com")
-		pinger.SetPrivileged(true)
-		if err != nil {
+	if *window%*interval != 0 {
+		log.Fatal("please let interval divide window evenly, thanks")
+	}
+	size := *window / *interval
+
+	pinger, err := probing.NewPinger(*target)
+	if err != nil {
+		log.Fatal(fmt.Errorf("new pinger: %w", err))
+	}
+
+	var lock sync.Mutex
+
+	sent := 0
+	recv := 0
+	measurements := make([]time.Duration, size)
+	pinger.SetPrivileged(true)
+	pinger.Interval = time.Duration(*interval) * time.Second
+	pinger.OnSend = func(p *probing.Packet) {
+		lock.Lock()
+		defer lock.Unlock()
+
+		for ; recv < sent; recv++ {
+			measurements[recv%size] = -1
+		}
+		sent++
+	}
+	pinger.OnRecv = func(p *probing.Packet) {
+		lock.Lock()
+		defer lock.Unlock()
+		if recv < sent {
+			measurements[recv%size] = p.Rtt
+		}
+		recv++
+	}
+
+	go func() {
+		if err := pinger.Run(); err != nil {
 			log.Fatal(err)
 		}
-		pinger.Count = 3
-		err = pinger.Run()
-		if err != nil {
-			log.Fatal(err)
+	}()
+
+	ds := fmtDur(time.Duration(*window) * time.Second)
+	pingName := fmt.Sprintf("pingtime_%s", ds)
+	lossName := fmt.Sprintf("pingloss_%s", ds)
+
+	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		lock.Lock()
+		defer lock.Unlock()
+
+		var sum time.Duration
+		var lost int
+		var have int
+		for _, val := range measurements {
+			switch {
+			case val < 0:
+				lost++
+			case val == 0:
+			default:
+				have++
+				sum += val
+			}
 		}
-		stats := pinger.Statistics()
+		// Average in seconds
+		avgRtt := float64(sum) / float64(have) / 1e9
+		lossRatio := float64(lost) / float64(have+lost)
 
 		_, _ = w.Write(
 			[]byte(fmt.Sprintf(
-				`# HELP uptime 
-# TYPE uptime gauge 
+				`# TYPE uptime gauge 
 uptime %f
 
-# HELP pingtime
-# TYPE pingtime gauge
-pingtime %f
+# TYPE %s gauge
+%s %f
+
+# TYPE %s gauge
+%s %f
 `,
 				time.Since(startTime).Seconds(),
-				stats.AvgRtt.Seconds(),
+				pingName, pingName,
+				avgRtt,
+				lossName, lossName,
+				lossRatio,
 			)))
 	})
 	if err := http.ListenAndServe(fmt.Sprint(":", *port), nil); err != nil {
