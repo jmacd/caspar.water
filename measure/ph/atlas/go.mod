a04a970c80c79c11ad6c7519f6d6ddff0f2d8bf8
Joshua MacDonald <jmacd@lightstep.com>
2024-02-10 22:14:56 -0800

    use mock test

--- measure/ph/atlas/go.mod
+++ measure/ph/atlas/go.mod
@@ -4,15 +4,18 @@ go 1.21
 
 require (
 	github.com/spf13/cobra v1.7.0
+	github.com/stretchr/testify v1.8.4
 	go.opentelemetry.io/collector/component v0.79.0
 	go.opentelemetry.io/collector/consumer v0.79.0
 	go.opentelemetry.io/collector/pdata v1.0.0-rcv0012
 	go.opentelemetry.io/collector/receiver v0.79.0
+	go.uber.org/mock v0.4.0
 	go.uber.org/zap v1.24.0
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -23,6 +26,7 @@ require (
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.opentelemetry.io/collector v0.79.0 // indirect
 	go.opentelemetry.io/collector/confmap v0.79.0 // indirect
@@ -38,4 +42,5 @@ require (
 	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- measure/ph/atlas/internal/ezo/ezo_test.go
+++ measure/ph/atlas/internal/ezo/ezo_test.go
@@ -1,62 +1,134 @@
 package ezo
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/jmacd/caspar.water/measure/ph/atlas/internal/device"
+	"github.com/jmacd/caspar.water/measure/ph/atlas/internal/device/mock"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
+func set(d []byte, n byte, s string) {
+	b := []byte(s)
+	d[0] = n
+	copy(d[1:1+len(b)], b)
+}
+
 func TestPhInfo(t *testing.T) {
-	tdev := &device.Fake{
-		Version: "1.23",
-	}
+	ctrl := gomock.NewController(t)
+	tdev := mock.NewMockI2C(ctrl)
+
 	ph := New(tdev)
 	require.NotNil(t, ph)
+
+	gomock.InOrder(
+		tdev.EXPECT().Write([]byte("i")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "?i,pH,1.23")
+			return nil
+		}))
 
 	info, err := ph.Info()
 	require.NoError(t, err)
-	require.Equal(t, info.Version, tdev.Version)
+	require.Equal(t, "1.23", info.Version)
 }
 
 func TestPhStatus(t *testing.T) {
-	tdev := &device.Fake{
-		Vcc:         "3.45",
-		RestartCode: "P",
-	}
+	ctrl := gomock.NewController(t)
+	tdev := mock.NewMockI2C(ctrl)
+
 	ph := New(tdev)
 	require.NotNil(t, ph)
 
+	gomock.InOrder(
+		tdev.EXPECT().Write([]byte("Status")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "?Status,P,3.83")
+			return nil
+		}))
+
 	stat, err := ph.Status()
 	require.NoError(t, err)
-	require.Equal(t, fmt.Sprint(stat.Vcc), tdev.Vcc)
+	require.Equal(t, stat.Vcc, 3.83)
 	require.Equal(t, stat.Restart, "Powered off")
 }
 
 func TestPhName(t *testing.T) {
-	tdev := &device.Fake{
-		Name: "test",
-	}
+	ctrl := gomock.NewController(t)
+	tdev := mock.NewMockI2C(ctrl)
+
 	ph := New(tdev)
 	require.NotNil(t, ph)
 
+	gomock.InOrder(
+		tdev.EXPECT().Write([]byte("Name,probe")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "")
+			return nil
+		}),
+		tdev.EXPECT().Write([]byte("Name,?")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "?Name,probe")
+			return nil
+		}),
+	)
+
+	err := ph.SetName("probe")
+	require.NoError(t, err)
+
 	name, err := ph.Name()
-	require.NoError(t, err)
-	require.Equal(t, "test", name)
-
-	err = ph.SetName("probe")
-	require.NoError(t, err)
-
-	name, err = ph.Name()
 	require.NoError(t, err)
 	require.Equal(t, "probe", name)
 }
 
 func TestCalibration(t *testing.T) {
-	tdev := &device.Fake{}
+	ctrl := gomock.NewController(t)
+	tdev := mock.NewMockI2C(ctrl)
+
 	ph := New(tdev)
 	require.NotNil(t, ph)
+
+	gomock.InOrder(
+		tdev.EXPECT().Write([]byte("Cal,?")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "?Cal,0")
+			return nil
+		}),
+
+		tdev.EXPECT().Write([]byte("Cal,mid,7.01")),
+		tdev.EXPECT().Sleep(device.Long),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "")
+			return nil
+		}),
+
+		tdev.EXPECT().Write([]byte("Cal,low,4.02")),
+		tdev.EXPECT().Sleep(device.Long),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "")
+			return nil
+		}),
+
+		tdev.EXPECT().Write([]byte("Cal,high,10.03")),
+		tdev.EXPECT().Sleep(device.Long),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "")
+			return nil
+		}),
+
+		tdev.EXPECT().Write([]byte("Cal,clear")),
+		tdev.EXPECT().Sleep(device.Short),
+		tdev.EXPECT().Read(gomock.Any()).DoAndReturn(func(d []byte) error {
+			set(d, 1, "")
+			return nil
+		}),
+	)
 
 	pts, err := ph.CalibrationPoints()
 	require.NoError(t, err)
@@ -64,25 +136,13 @@ func TestCalibration(t *testing.T) {
 
 	err = ph.CalibrateMidpoint(7.01)
 	require.NoError(t, err)
-	pts, err = ph.CalibrationPoints()
-	require.NoError(t, err)
-	require.Equal(t, 1, pts)
 
 	err = ph.CalibrateLowpoint(4.02)
 	require.NoError(t, err)
-	pts, err = ph.CalibrationPoints()
-	require.NoError(t, err)
-	require.Equal(t, 2, pts)
 
 	err = ph.CalibrateHighpoint(10.03)
 	require.NoError(t, err)
-	pts, err = ph.CalibrationPoints()
-	require.NoError(t, err)
-	require.Equal(t, 3, pts)
 
 	err = ph.ClearCalibration()
 	require.NoError(t, err)
-	pts, err = ph.CalibrationPoints()
-	require.NoError(t, err)
-	require.Equal(t, 0, pts)
 }
