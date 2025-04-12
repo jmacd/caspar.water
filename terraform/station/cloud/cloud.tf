1de8561708d59d7fc65d2109a3e59619b119a57b
Joshua MacDonald <josh.macdonald@gmail.com>
2025-04-12 16:34:54 -0700

    add site outline

--- terraform/station/cloud/cloud.tf
+++ terraform/station/cloud/cloud.tf
@@ -16,7 +16,7 @@ provider "linode" {
   token = var.li_token
 }
 
-resource "linode_instance" "debian-us-west" {
+resource "linode_instance" "debian12-us-west" {
   region = "us-west"
   type = "g6-nanode-1"
 }
@@ -27,7 +27,7 @@ resource "null_resource" "setup-script" {
       type     = "ssh"
       user     = "root"
       private_key="${file("/Users/jmacd/.ssh/id_rsa")}"
-      host     = linode_instance.debian-us-west.ip_address
+      host     = linode_instance.debian12-us-west.ip_address
   }
 
   provisioner "file" {
@@ -36,44 +36,29 @@ resource "null_resource" "setup-script" {
   }
 
   provisioner "file" {
-      source      = "bridge.d"
-      destination = "/etc/sysctl.d/bridge.d"
+      source      = "casparwater_certs/casparwater_us.key"
+      destination = "/etc/casparwater/casparwater_us.key"
   }
 
   provisioner "file" {
-      source      = "nomadserver.service"
-      destination = "/etc/systemd/system/nomadserver.service"
+      source      = "casparwater_certs/casparwater_us.crt"
+      destination = "/etc/casparwater/casparwater_us.crt"
   }
 
   provisioner "file" {
-      source      = "nomadclient.service"
-      destination = "/etc/systemd/system/nomadclient.service"
+      source      = "nginx.casparwater.conf"
+      destination = "/etc/nginx/sites-enabled/casparwater"
   }
 
   provisioner "file" {
-      source      = "nomadserver.hcl"
-      destination = "/etc/nomad.d/nomadserver.hcl"
+      source      = "../../../site/"
+      destination = "/var/www/html/"
   }
 
-  provisioner "file" {
-      source      = "nomadclient.hcl"
-      destination = "/etc/nomadclient.d/nomadclient.hcl"
-  }
-
-  provisioner "file" {
-      source      = "influxdb-vars.hcl"
-      destination = "/etc/caspar.d/influxdb/vars.hcl"
-  }
-
-  provisioner "file" {
-      source      = "influxdb.yaml"
-      destination = "/etc/caspar.d/influxdb/config.yaml"
-  }
-
-  provisioner "file" {
-      source      = "nginx.conf"
-      destination = "/etc/nginx/nginx.conf"
-  }
+  #provisioner "file" {
+  #    source      = "influxdb.yaml"
+  #    destination = "/etc/caspar.d/influxdb/config.yaml"
+  #}
 
   provisioner "remote-exec" {
     inline = [
