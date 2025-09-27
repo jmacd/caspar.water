8f560c87c8ec6329ef11220c6bd8f3cfe88f9b34
Joshua MacDonald <josh.macdonald@gmail.com>
2025-09-27 11:07:51 -0700

    better

--- datacp.sh
+++ datacp.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+echo From water
+
 rsync -chavzP --stats jmacd@linux.local:/home/data /Volumes/sourcecode/src/caspar.water
+
+echo To cloud
+
+rsync -chavzP --stats /Volumes/sourcecode/src/caspar.water/data root@casparwater.us:/home
